911c6d124314db91da0edc1f931d7a0d3aa96f1f
yankyalvarezm <alvarezyanky7#gmail.com>
2023-11-25 18:43:33 -0500

    added deployments links

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,5 +1,7 @@
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://cantico-server.adaptable.app";
 
-const TEST_HOST = "http://localhost:5173";
+// const TEST_HOST = "http://localhost:5173";
+const TEST_HOST = "https://cantico-server.adaptable.app";
 
 export { API_URL, TEST_HOST };
