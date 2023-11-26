7a9895d265b4b7d7432798056b7ca57f7d1dbe71
yankyalvarezm <alvarezyanky7#gmail.com>
2023-11-25 21:52:28 -0500

    added validation

--- src/components/WhosNext.jsx
+++ src/components/WhosNext.jsx
@@ -1,45 +1,52 @@
-import React, { useContext, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
-import { useSongs } from '../context/Songs.context'; 
-import { AuthContext } from '../context/auth.context'; // Asegúrate de importar AuthContext
-import DeleteMySong from './DeleteMySong';
+import React, { useContext, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { useSongs } from "../context/Songs.context";
+import { AuthContext } from "../context/auth.context"; // Asegúrate de importar AuthContext
+import DeleteMySong from "./DeleteMySong";
 
 const WhosNext = () => {
-    const { queueSongs, refreshQueueSongs, activeSession } = useSongs();
-    const { user } = useContext(AuthContext); 
+  const { queueSongs, refreshQueueSongs, activeSession } = useSongs();
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        if (activeSession && activeSession._id) {
-            refreshQueueSongs(activeSession._id); 
-        }
-    }, [activeSession, refreshQueueSongs]);
+  useEffect(() => {
+    if (activeSession && activeSession._id) {
+      refreshQueueSongs(activeSession._id);
+    }
+  }, [activeSession, refreshQueueSongs]);
 
-    return (
-        <div>
-            <Button variant="dark" className='mysongs-btn'>Queue</Button>
-            {queueSongs.map((song, index) => (
-               <div
-               key={song._id}
-               className={`queue-song-item ${index === 0 ? 'song-playing' : ''} ${index === 1 ? 'song-next' : ''} ${user && !user.admin && song.user._id !== user._id && index > 1? 'default-song' : ''}`}
-           >
-               {index === 0 && <p className="song-status">Sonando</p>}
-               {index === 1 && <p className="song-status">Siguiente en fila</p>}
-           
-               <h3>{song.user.name}</h3>
-               <p>{song.name}</p>
-           
-               {user && (user.admin || song.user._id === user._id) && (
-                   <DeleteMySong
-                       perfomId={song._id}
-                       activeSession={activeSession}
-                       onRefresh={() => refreshQueueSongs(activeSession._id)}
-                   />
-               )}
-           </div>
-           
-            ))}
+  return (
+    <div>
+      <Button variant="dark" className="mysongs-btn">
+        Queue
+      </Button>
+      {queueSongs.map((song, index) => (
+        <div
+          key={song._id}
+          className={`queue-song-item ${index === 0 ? "song-playing" : ""} ${
+            index === 1 ? "song-next" : ""
+          } ${
+            user && !user.admin && song.user._id !== user._id && index > 1
+              ? "default-song"
+              : ""
+          }`}
+        >
+          {index === 0 && <p className="song-status">Sonando</p>}
+          {index === 1 && <p className="song-status">Siguiente en fila</p>}
+
+          <h3>{user ? song.user.name : ""}</h3>
+          <p>{song.name}</p>
+
+          {user && (user.admin || song.user._id === user._id) && (
+            <DeleteMySong
+              perfomId={song._id}
+              activeSession={activeSession}
+              onRefresh={() => refreshQueueSongs(activeSession._id)}
+            />
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default WhosNext;
