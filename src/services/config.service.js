cd05a7cc77a6b7cec2fe98abb85b70f05964ed57
alextor26 <alextor2625@gmail.com>
2023-11-27 20:22:01 -0400

    Changed Api Url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -3,5 +3,6 @@ const API_URL = "https://cantico-server.adaptable.app";
 
 // const TEST_HOST = "http://localhost:5173";
 const TEST_HOST = "https://cantico.netlify.app";
+//
 
 export { API_URL, TEST_HOST };
