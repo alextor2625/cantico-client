16c4ba924cffc96bb7bda4fe01d6cd7c1df7c15e
John Doe <johndoe@example.com>
2023-12-13 15:30:45 -0400

    change host

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,8 +1,8 @@
-const API_URL = "http://localhost:3000";
-// const API_URL = "https://cantico-server.adaptable.app";
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://cantico-server.adaptable.app";
 
-const TEST_HOST = "http://localhost:5173";
-// const TEST_HOST = "https://cantico.netlify.app";
+// const TEST_HOST = "http://localhost:5173";
+const TEST_HOST = "https://cantico.netlify.app";
 const hola = 'hello';
 
 export { API_URL, TEST_HOST };
