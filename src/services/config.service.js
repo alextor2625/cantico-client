df0efb6a7d66483f6e2d182d0d6dbb1c4f82de6b
John Doe <johndoe@example.com>
2023-12-13 13:37:27 -0400

    change the backend

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,5 +1,7 @@
 // const API_URL = "http://localhost:3000";
-const API_URL = "https://cantico-server.adaptable.app";
+// const API_URL = "https://cantico-server.adaptable.app";
+
+const API_URL = "cantico-server.netlify.app";
 
 // const TEST_HOST = "http://localhost:5173";
 const TEST_HOST = "https://cantico.netlify.app";
