9775807c201903e7e5a134d1c2276e7fee5445ca
John Doe <johndoe@example.com>
2023-12-13 15:29:48 -0400

    added something to test

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -3,5 +3,6 @@ const API_URL = "http://localhost:3000";
 
 const TEST_HOST = "http://localhost:5173";
 // const TEST_HOST = "https://cantico.netlify.app";
+const hola = 'hello';
 
 export { API_URL, TEST_HOST };
